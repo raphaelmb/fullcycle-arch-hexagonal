f418831c00c2ad0ec22015c6055c90c54ae2863a
Raphael Berger <raphaelmb@gmail.com>
2022-08-03 16:41:03 -0300

    Add cli

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	_ "github.com/mattn/go-sqlite3"
-	db2 "github.com/raphaelmb/fullcycle-arch-hexagonal/adapters/db"
-	"github.com/raphaelmb/fullcycle-arch-hexagonal/application"
-)
+import "github.com/raphaelmb/fullcycle-arch-hexagonal/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product", 30)
-
-	productService.Enable(product)
+	cmd.Execute()
 }
